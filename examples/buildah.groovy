ae08e10dec3e667c0b7e4e67401230c251044fcf
Yagmur Guraslan <yagmur.guraslan@ntoklo.com>
2019-10-28 15:59:52 +0000

    initial commit

---
+++ examples/buildah.groovy
@@ -0,0 +1,25 @@
+podTemplate(containers: [
+    containerTemplate(name: 'buildah', image: 'centos:centos7', ttyEnabled: true, command: 'cat'),
+  ]) {
+
+    node(POD_LABEL) {
+
+        stage('Create image with Buildah') {
+            git url: 'https://github.com/alpine-docker/helm.git'
+            container('buildah') {
+                staged('Install buildah') {
+                    sh """
+                    yum -y install buildah
+                    """
+                }
+                stage('Build alpine-helm image') {
+                    sh """
+                    id
+                    buildah bud -f Dockerfile --build-arg VERSION=2.12.0 -t alpine-help:2.12.0 .
+                    """
+                }
+            }
+        }
+
+    }
+}
