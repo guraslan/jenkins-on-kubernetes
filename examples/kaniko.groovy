c797c13bbc9c546126d5fa976afc0cfff0de9fe9
Yagmur Guraslan <yagmur.guraslan@ntoklo.com>
2019-10-30 17:22:31 +0000

    kaniko pipeline example

---
+++ examples/kaniko.groovy
@@ -0,0 +1,58 @@
+pipeline {
+  environment {
+      PROJECT=stalwart-topic-257411
+      IMAGE=alpine-helm
+      TAG=0.1
+  }
+  agent {
+    kubernetes {
+      defaultContainer 'jnlp'
+      yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  name: kaniko
+spec:
+  containers:
+  - name: maven
+    image: maven:alpine
+    command:
+    - cat
+    tty: true
+  - name: busybox
+    image: busybox
+    command:
+    - cat
+    tty: true
+  - name: kaniko
+    image: gcr.io/kaniko-project/executor:latest
+    args: ["--dockerfile=Dockerfile",
+            "--context=dir://.",
+            "--destination=gcr.io/$PROJECT/$IMAGE:$TAG"]
+    volumeMounts:
+      - name: kaniko-secret
+        mountPath: /secret
+    env:
+      - name: GOOGLE_APPLICATION_CREDENTIALS
+        value: /secret/kaniko-secret.json
+  restartPolicy: Never
+  volumes:
+    - name: kaniko-secret
+      secret:
+        secretName: kaniko-secret
+"""
+    }
+  }
+  stages {
+    stage('Run maven') {
+      steps {
+        container('maven') {
+          sh 'mvn -version'
+        }
+        container('busybox') {
+          sh '/bin/busybox'
+        }
+      }
+    }
+  }
+}
